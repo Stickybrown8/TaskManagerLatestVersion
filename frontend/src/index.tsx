f6737817b8c81180456770c081619a2056bc1e82
Stickybrown8 <contact@digitalmarketing-beyond.com>
2025-04-23 16:30:08 +0200

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,64 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './store';
-import ClientForm from './pages/ClientForm';
-import Profile from './pages/Profile';
 import './index.css';
 
 // Composant ErrorBoundary pour capturer les erreurs
 import ErrorBoundary from './components/ErrorBoundary';
-
-// Composants de mise en page
-import Layout from './components/Layout';
-import PrivateRoute from './components/PrivateRoute';
-import TimerPopup from './components/timer/TimerPopup';
 import App from './App';
-
-// Pages
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Tasks from './pages/Tasks';
-import TaskDetail from './pages/TaskDetail';
-import TaskForm from './pages/TaskForm';  // Ajoutez cette ligne
-import Clients from './pages/Clients';
-import ClientDashboard from './pages/ClientDashboard';  // Ajoutez cette ligne
-import Gamification from './pages/Gamification';
-import TestApi from './pages/TestApi';
-import TestLogin from './TestLogin';
-import AdminSetup from './AdminSetup';
 
 const Root = () => {
   return (
     <ErrorBoundary>
-      <App>
-        {/* Le timer est toujours disponible, indépendamment des routes */}
-        <TimerPopup />
-        
-        <Routes>
-          {/* Routes publiques */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/test-api" element={<TestApi />} />
-          <Route path="/test-login" element={<TestLogin />} />
-          <Route path="/admin-setup" element={<AdminSetup />} />
-          
-          {/* Routes privées avec layout */}
-          <Route element={<PrivateRoute />}>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/clients" element={<Clients />} />
-              <Route path="/clients/new" element={<ClientForm />} />
-              <Route path="/tasks" element={<Tasks />} />
-              <Route path="/tasks/new" element={<TaskForm />} />  {/* Ajoutez cette ligne */}
-              <Route path="/tasks/:id" element={<TaskDetail />} />
-              <Route path="/dashboard/clients" element={<ClientDashboard />} />  {/* Ajoutez cette ligne */}
-              <Route path="/gamification" element={<Gamification />} />
-              <Route path="/profile" element={<Profile />} />
-            </Route>
-          </Route>
-        </Routes>
-      </App>
+      <App />
     </ErrorBoundary>
   );
 };
