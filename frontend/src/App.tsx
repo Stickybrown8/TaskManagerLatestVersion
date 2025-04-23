a0e83fba736c8a2f5d4f2abb19a0d70483a161fa
Stickybrown8 <contact@digitalmarketing-beyond.com>
2025-04-23 16:30:42 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,56 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Composants de mise en page
+import Layout from './components/Layout';
 import PrivateRoute from './components/PrivateRoute';
-import Clients from './pages/Clients';
+import TimerPopup from './components/timer/TimerPopup';
+
+// Pages
 import Login from './pages/Login';
-// Ajoute ici d'autres pages
+import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
+import TaskDetail from './pages/TaskDetail';
+import TaskForm from './pages/TaskForm';
+import Clients from './pages/Clients';
+import ClientForm from './pages/ClientForm';
+import ClientDashboard from './pages/ClientDashboard';
+import Gamification from './pages/Gamification';
+import Profile from './pages/Profile';
+import TestApi from './pages/TestApi';
+import TestLogin from './TestLogin';
+import AdminSetup from './AdminSetup';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <Routes>
-      {/* Route publique */}
-      <Route path="/login" element={<Login />} />
+    <>
+      {/* Le timer est toujours disponible, indépendamment des routes */}
+      <TimerPopup />
 
-      {/* Toutes les routes privées passent ici */}
-      <Route element={<PrivateRoute />}>
-        <Route path="/clients" element={<Clients />} />
-        {/* Ajoute d'autres routes privées ici */}
-      </Route>
+      <Routes>
+        {/* Routes publiques */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/test-api" element={<TestApi />} />
+        <Route path="/test-login" element={<TestLogin />} />
+        <Route path="/admin-setup" element={<AdminSetup />} />
 
-      {/* Route par défaut ou 404, facultatif */}
-      {/* <Route path="*" element={<NotFound />} /> */}
-    </Routes>
+        {/* Routes privées avec layout */}
+        <Route element={<PrivateRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/clients" element={<Clients />} />
+            <Route path="/clients/new" element={<ClientForm />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/tasks/new" element={<TaskForm />} />
+            <Route path="/tasks/:id" element={<TaskDetail />} />
+            <Route path="/dashboard/clients" element={<ClientDashboard />} />
+            <Route path="/gamification" element={<Gamification />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
