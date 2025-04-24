bc200658ae89788a97a7575c1749a6f5cc2e1c36
Stickybrown8 <contact@digitalmarketing-beyond.com>
2025-04-24 14:20:43 +0200

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
-import clientsReducer from './store/slices/authSlice';
+import clientsReducer from './slices/authSlice';
 import tasksReducer from './slices/taskSlice';
 
 export const store = configureStore({
