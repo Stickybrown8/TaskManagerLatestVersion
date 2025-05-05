131f1c293066c6790dcd5b5a8340aee2c876398b
Stickybrown8 <contact@digitalmarketing-beyond.com>
2025-05-05 14:08:49 +0000

    Correction du chemin SVG dans le bouton de fermeture du TimerPopupFix

--- frontend/src/components/timer/TimerPopupFix.tsx
+++ frontend/src/components/timer/TimerPopupFix.tsx
@@ -668,7 +668,7 @@ const runningTimer = useAppSelector(state => state.timer?.runningTimer || null);
               title="Fermer"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 011.414 1.414L11.414 10l4.293 4.293a1 1 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
               </svg>
             </button>
           </div>
